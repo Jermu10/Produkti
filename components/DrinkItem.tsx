f066583eb0e0fd0e2d648d18e0355666a93eafe4
Jeremy <jerkku_astrom@hotmail.com>
2024-10-29 17:14:20 +0200

    Updating slugs

--- components/DrinkItem.tsx
+++ components/DrinkItem.tsx
@@ -9,7 +9,7 @@ import { formatIngredients } from "@/lib/utils";
 const DrinkItem: React.FC<DrinkItemProps> = ({ drink }) => {
   return (
     <li key={drink.id} className="border-b pb-4">
-      <Link href={`/admin/drinks/${drink.id}`}>
+      <Link href={`/admin/drinkit/${drink.id}`}>
         <div className="block text-center">
           <h2 className="text-2xl font-semibold">{drink.name}</h2>
           <div className="mt-2 whitespace-pre-wrap">
