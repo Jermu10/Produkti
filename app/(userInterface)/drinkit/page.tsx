8b6081487df1eb534ac2b6cf31442a18b56d7024
Jeremy <jerkku_astrom@hotmail.com>
2024-11-06 20:40:14 +0200

    export error fixed

--- app/(userInterface)/drinkit/page.tsx
+++ app/(userInterface)/drinkit/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { getUserDrinks } from "../../actions/drink.actions";
 import Header from "@/components/Header";
 
-export const DrinksPage = async () => {
+const DrinksPage = async () => {
   const drinks = await getUserDrinks();
 
   return (
